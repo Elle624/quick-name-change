5c9f07eb3c0e5eb4b89a710d78ec8b290952d1e1
Yiran Li <yiran.li@hotmail.com>
2020-12-21 17:51:30 -0700

    Add useRef to input field

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { UserContextConsumer } from '../../userContext';
 import Header from '../Header';
 
 const App = () => {
   const [newUserName, setUserName] = useState('');
+  const inputRef = useRef(null);
 
   const updateUserName = (event) => {
     setUserName(event.target.value);
@@ -18,6 +19,7 @@ const App = () => {
           <main>
             <p className='main'> 🗣 No new notifications, {userName}! 🎉</p>
             <input
+              ref={inputRef}
               type='text'
               name='newUsername'
               placeholder='New username'
@@ -28,6 +30,8 @@ const App = () => {
               onClick={() => {
                 changeUserName(newUserName);
                 setUserName('');
+                inputRef.current.disabled = false;
+                inputRef.current.focus();
               }}
             >
               Change Username
